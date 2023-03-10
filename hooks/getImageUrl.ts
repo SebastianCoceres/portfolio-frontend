d9d701d4101c68b82611267065b5b702f374ce8d
Sebastian Coceres <sebastian.coceres11@gmail.com>
2023-03-10 16:07:30 +0100

    update workflows and secrets

--- hooks/getImageUrl.ts
+++ hooks/getImageUrl.ts
@@ -2,7 +2,7 @@ import images from "../constants/images";
 
 const getImageUrl = (url: any) => {
   if (url) {
-    return `http://localhost:1337${url}`;
+    return `${process.env.MY_API_IMAGES}${url}`;
   } else {
     return images.dummy.src;
   }
