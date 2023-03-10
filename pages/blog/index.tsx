1f4feddf9b17d83f53f47a65a98b6a644fd1a1d2
Sebastian Coceres <sebastian.coceres11@gmail.com>
2023-03-10 23:05:46 +0100

    blogs search fix

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -70,7 +70,7 @@ const Blogs: NextPage = ({
         <meta name="og:title" content={title} />
         <meta name="og:description" content={description} />
       </Head>
-      <section className="container mx-auto py-24 text-white">
+      <section className="container mx-auto py-24 px-4 text-white">
         <motion.div
           layoutId="blogImg"
           initial={{ opacity: 0 }}
